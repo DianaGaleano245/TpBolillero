f18296901164ea5de7bb10ba61417e013c56674f
DianaGaleano245 <emmaymaggi@gmail.com>
2022-04-20 15:37:31 -0300

    avion01

--- Tp1Bolillero/Bolillero.cs
+++ Tp1Bolillero/Bolillero.cs
@@ -1,11 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Tp1Bolillero
 {
     public class Bolillero
     {
-        public byte Adentro {get;set;}
-        public byte Afuera {get;set;}
-        public byte Azar {get;set;}
+        public List<byte> Adentro { get; set; }
+        public List<byte> Afuera { get; set; }
+        public IAzar Azar { get; set; }
+
+        public Bolillero(IAzar azar, byte cantidad)
+        {
+            Azar = azar;
+            Adentro = new List<byte>();
+            Afuera = new List<byte>();
+            CrearBolilla(cantidad);
+        }
+        public void CrearBolilla(byte bolillas)
+        {
+            for (byte i = 0; i < bolillas; i++)
+            {
+                Adentro.Add(i);
+            }
+        }
     }
 }
