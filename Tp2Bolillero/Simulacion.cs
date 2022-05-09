eac9506ae500e24687d5737dd87a80ac2de7c8ca
luchoxx87 <emmaymaggi@gmail.com>
2022-05-09 17:09:34 -0300

    hi

---
+++ Tp2Bolillero/Simulacion.cs
@@ -0,0 +1,13 @@
+namespace Tp2Bolillero
+{
+    public class Simulacion
+    {
+        private long simularHilos(List<byte>bolillas,int cantidad);
+
+        public bool Jugar(List<byte> bolillas);
+
+
+        private long simularConHilos(List<byte>bolillas,int cantidad);
+
+    }
+}
