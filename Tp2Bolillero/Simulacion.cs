a2aea6d4704acc003f4c6d32a776516c56eb2bb1
luchoxx87 <emmaymaggi@gmail.com>
2022-05-16 17:20:48 -0300

    lalaland

--- Tp2Bolillero/Simulacion.cs
+++ Tp2Bolillero/Simulacion.cs
@@ -2,10 +2,21 @@ namespace Tp2Bolillero
 {
     public class Simulacion
     {
-        private long simularHilos(List<byte>bolillas,int cantidad);
+        private long simularHilos(List<byte>bolillas,int cantidad, int jugada);
+        public void bolillas()
+        {
+            for (int i = 0; i < bolillas.Count; i++)
+            {
+            if(SacarBolilla() == bolillas[i])
+                return true;
+            }
+            return false;
+        }
+        public void cantidad()
+        {
 
+        }
         public bool Jugar(List<byte> bolillas);
-
 
         private long simularConHilos(List<byte>bolillas,int cantidad);
 
