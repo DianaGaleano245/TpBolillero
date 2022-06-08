b14f5d412306b340c7be6976b45a76c8579d8b0a
DianaGaleano245 <emmaymaggi@gmail.com>
2022-06-08 14:22:58 -0300

    Terminamos segunda interaccion

--- Tp1Bolillero/Simulacion .cs
+++ Tp1Bolillero/Simulacion .cs
@@ -10,15 +10,18 @@ namespace Tp1Bolillero
         {
             return bolillero.JugarN(jugada, cantidad);
         }
-        private long simularConHilos(Bolillero bolillero, List<byte> jugada, long cantidadSimulaciones, long cantidadHilos)
+        private long simularConHilos(Bolillero bolillero, List<byte> jugada, long cantidadSimulaciones, long cantidadporHilos)
         {
             long cantidadporhilos = cantidadSimulaciones/cantidadHilos;
-            var tareas = new Task<long>[cantidadHilos];
-            for (int i = 0; i < cantidadSimulaciones; i++)
+            var tareas = new Task<long>[cantidadSimulaciones];
+            for (int i = 0; i < cantidadporHilos; i++)
             {
                 Bolillero clon = (Bolillero)bolillero.Clone();
                 tareas[i] = Task.Run(() => simularSinHilos(clon, jugada, cantidadSimulaciones));
             }
+            Task<int>.WaitAll(cantidadporHilos);
+            return tareas.Sum(i => { 
+        }); 
         }
     }
 }
